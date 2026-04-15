5bed60c4a1c82e75508646ccf747e44e3886d041
pialtor <29535559+pialtor@users.noreply.github.com>
2026-04-15 15:42:24 +0300

    fix: expose tg-ws-proxy attribute

--- flake.nix
+++ flake.nix
@@ -10,47 +10,47 @@
     };
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      tg-ws-proxy-src,
-    }:
-    let
-      forAllSystems = f: nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ] f;
+  outputs = {
+    self,
+    nixpkgs,
+    tg-ws-proxy-src,
+  }: let
+    forAllSystems = f: nixpkgs.lib.genAttrs ["x86_64-linux" "aarch64-linux"] f;
 
-      pkgsFor = system: import nixpkgs { inherit system; };
-    in
-    {
-      packages = forAllSystems (
-        system:
-        let
-          pkgs = pkgsFor system;
-          pythonEnv = pkgs.python3.withPackages (
-            ps: with ps; [
+    pkgsFor = system: import nixpkgs {inherit system;};
+  in {
+    packages = forAllSystems (
+      system: let
+        pkgs = pkgsFor system;
+        pythonEnv = pkgs.python3.withPackages (
+          ps:
+            with ps; [
               websockets
               requests
               cryptography
             ]
-          );
-        in
-        {
-          default = pkgs.writeShellApplication {
-            name = "tg-ws-proxy";
-            runtimeInputs = [ pythonEnv ];
-            text = ''
-              export PYTHONPATH="${tg-ws-proxy-src}"
-              exec python ${tg-ws-proxy-src}/proxy/tg_ws_proxy.py "$@"
-            '';
-          };
-        }
-      );
-
-      apps = forAllSystems (system: {
-        default = {
-          type = "app";
-          program = "${self.packages.${system}.default}/bin/tg-ws-proxy";
+        );
+      in {
+        default = pkgs.writeShellApplication {
+          name = "tg-ws-proxy";
+          runtimeInputs = [pythonEnv];
+          text = ''
+            export PYTHONPATH="${tg-ws-proxy-src}"
+            exec python ${tg-ws-proxy-src}/proxy/tg_ws_proxy.py "$@"
+          '';
         };
-      });
-    };
+
+        tg-ws-proxy = self.packages.${system}.default;
+      }
+    );
+
+    apps = forAllSystems (system: {
+      default = {
+        type = "app";
+        program = "${self.packages.${system}.default}/bin/tg-ws-proxy";
+      };
+
+      tg-ws-proxy = self.apps.${system}.default;
+    });
+  };
 }
